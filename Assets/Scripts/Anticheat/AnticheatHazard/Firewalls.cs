ba3f1a2d83d7e2d5fbd4b240c12fcabf637f0677
Victor <avn@sfu.ca>
2026-03-24 13:48:20 -0700

    Made texture for firewall

--- Assets/Scripts/Anticheat/AnticheatHazard/Firewalls.cs
+++ Assets/Scripts/Anticheat/AnticheatHazard/Firewalls.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class Firewalls : MonoBehaviour
 {
-    void Update()
+    private bool wasActive = false;
+    private GameObject antiCheat=null;
+    void Start()
     {
-        StunAnticheat anticheat = Object.FindFirstObjectByType<StunAnticheat>();
-        bool shouldBeVisible = anticheat != null && anticheat.enabled;
+        antiCheat = GameObject.FindWithTag("Anticheat");
+        bool isCurrentlyActive = (antiCheat != null);
         foreach (Transform child in transform)
         {
-            if (child.gameObject.activeSelf != shouldBeVisible)
+            child.gameObject.SetActive(isCurrentlyActive);
+        }
+    }
+    void Update()
+    {
+        antiCheat = GameObject.FindWithTag("Anticheat");
+        bool isCurrentlyActive = (antiCheat != null);
+        if (isCurrentlyActive != wasActive)
+        {
+            wasActive = isCurrentlyActive;
+            
+            foreach (Transform child in transform)
             {
-                child.gameObject.SetActive(shouldBeVisible);
+                child.gameObject.SetActive(isCurrentlyActive);
             }
+            
         }
     }
 }
